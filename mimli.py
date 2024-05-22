ad820de2215d8b214ff8209d7d08d807539dbfee
MittatoTato <cumfilledmidgets@gmail.com>
2024-05-22 08:31:51 +0100

    Update mimli.py

--- mimli.py
+++ mimli.py
@@ -3,12 +3,30 @@ import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, TextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
 import zipfile
 import os
-import gdown
+import requests
 from keep_alive import keep_alive
 keep_alive()
 url = 'https://drive.google.com/uc?id=1umM2MDzlvZbo7_850yqY5YWVXRjt2FHS'
 output = './yes.zip'
-if not os.path.exists(output): gdown.download(url, output, quiet=False)
+
+def download_file_from_google_drive(shareable_link, destination):
+    # Extract the file ID from the shareable link
+    file_id = shareable_link.split('/d/')[1].split('/')[0]
+
+    # Construct the direct download URL
+    download_url = f'https://drive.google.com/uc?export=download&id={file_id}'
+
+    # Make a GET request to fetch the file
+    response = requests.get(download_url, stream=True)
+
+    # Save the file content to the destination file
+    with open(destination, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                file.write(chunk)
+
+    print(f'File downloaded successfully to {destination}')
+if not os.path.exists(output): download_file_from_google_drive(url, output)
 if not os.path.exists("./content/gpt2-finetuned"):
     with zipfile.ZipFile(output, 'r') as zip_ref:
         zip_ref.extractall(".")
